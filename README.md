bfa2f7830a69f12985f6ed9e7c4782904a71c798
gambastian <sebas.gamba02@gmail.com>
2016-08-15 21:00:46 -0500

    creacion de database

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 2016_II_OlimpiColombia
 Git repository for OlimpiColombia Project
+
+## Database
+
+* Install database
+
+> psql -U postgres -W -f install-db.sql
---
+++ sql/install-db.sql
@@ -0,0 +1,12 @@
+-- Database: olimpco
+
+-- DROP DATABASE olimpco;
+
+CREATE DATABASE olimpco
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Colombia.1252'
+       LC_CTYPE = 'Spanish_Colombia.1252'
+       CONNECTION LIMIT = -1;
+
