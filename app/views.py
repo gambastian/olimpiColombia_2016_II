495ef5afd20253fa01e64341d6f56c1568040e2c
Japerea912 <george5015@gmail.com>
2016-08-15 23:40:48 -0500

    creacion de metodo para obtencion de deportes y esqueleto de vista

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
+from app.models import Deporte
 from django.shortcuts import render
 
 # Create your views here.
+
+#Funcion para obtener los deportes para desplegar en el index
+def index(request):
+    lista_deportes = Deporte.objects.all()
+    context = {'lista_deportes': lista_deportes}
+    return render(request, 'app/index.html', context)
