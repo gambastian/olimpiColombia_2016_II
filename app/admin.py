d54a55fbe42b30ca83cc236ba7850e0cc91805f2
gambastian <sebas.gamba02@gmail.com>
2016-08-15 23:21:02 -0500

    Modulos de administracion para las tablas

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Deporte)
+admin.site.register(Deporte_Deportista)
+admin.site.register(Deportista)
+admin.site.register(Evento)
+admin.site.register(Destacado)
