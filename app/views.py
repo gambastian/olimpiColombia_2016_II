31c1184576609b89f51bc023aedf208b154b36c4
Japerea912 <george5015@gmail.com>
2016-08-21 19:58:51 -0500

    Creacion pantalla de video destacado de deportista

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from app.models import Deporte, Deporte_Deportista
+from app.models import Deporte, Deporte_Deportista, Destacado
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -8,3 +9,9 @@ def index(request):
     lista_deporte = Deporte.objects.all()
     context = {'lista_deporte': lista_deporte}
     return render(request, 'app/index.html', context)
+
+#Funcion para obtener el url de un video para un deportista en especifico
+def destacado_detail(request, deportista_id):
+    destacado_actual = get_object_or_404(Destacado.objects.filter(deportista__id=deportista_id))
+    context = {'destacado_actual' : destacado_actual}
+    return render(request, 'app/destacado.html', context)
